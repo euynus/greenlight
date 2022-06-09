d9bd60962a850fd427ca6300518c504aa0836e6c
suny <me@suny.cloud>
2022-06-09 17:39:47 +0800

    Finish chapter 3

---
+++ cmd/api/helpers.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"strconv"
+)
+
+// Retrieve the "id" URL parameter from the current request context, the convert it to
+// an integer and return it, If the operation isn't successful, return 0 and an error.
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid id parameter")
+	}
+
+	return id, nil
+
+}
+
+// Define an envelope type
+type envelope map[string]interface{}
+
+// Define a writeJSON() helper for sending response. This takes the destination
+// http.ResponseWriter, the HTTP status code to send, the data to encode to JSON, and a
+// header map containing any additional HTTP headers we want to include in the response.
+func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
+	// Encode the data to JSON, returning the error if there was one
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
+}
---
+++ cmd/api/movies.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"github.com/euynus/greenlight/internal/data"
+	"net/http"
+	"time"
+)
+
+// Add a createMovieHandler for the "POST /v1/movies" endpoint. For now we simply
+// return a plain-text placeholder response.
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create a new movie")
+}
+
+// Add a showMovieHandler for the "GET /v1/movies/:id" endpoint. For now, we retrieve
+//the interpolated "id" parameter from the current URL and include it in a placeholder
+//response.
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
